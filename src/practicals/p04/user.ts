470d0cede1781be8d6e6f97470bb0e3ecc973c62
68010417 <68010417@kmitl.ac.th>
2026-02-16 09:55:18 +0700

    p04_1

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { parentPort } from "node:worker_threads";
+
+export class User {
+    username : string;
+    private password :string;
+    private static LOGIN_ATTEMPTS = 0;
+
+    constructor(name:string,password:string){
+        this.username = name;
+        this.password = password;
+    }
+    login(password:string){
+        User.LOGIN_ATTEMPTS += 1;
+        return this.validatePassword(password);
+    }
+    private validatePassword(password: string): boolean {
+    return this.password === password
+  }
+    getLoginAttempts(){
+        return User.LOGIN_ATTEMPTS;
+    }
+}
