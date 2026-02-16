19d3b618893f878f0efa5358a6f90e05ef41c0cc
68010417 <68010417@kmitl.ac.th>
2026-02-16 09:25:55 +0700

    p02_1

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,17 @@
-export class Person {}
+export class Person {
+    firstname : string = "John";
+    lastname : string = "Doe";
+    age : number = 25;
+    static COUNTRY : string = "Thailand";
+
+    getFullName(){
+        return `${this.firstname} ${this.lastname}`;
+        
+    }
+    setAge(age:number){
+        return this.age = age;
+    }
+    getAge(){
+        return this.age
+    }
+}
