69984217132e14fc5bc6bf6483386c2d2e836ae8
68010417 <68010417@kmitl.ac.th>
2026-02-16 09:37:04 +0700

    p03_1

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,10 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+    generation:string;
+
+  constructor(name:string,generation:string,price:number){
+    super(name,price)
+        this.generation = generation;
+    }
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,18 @@
-export class Product {}
+export class Product {
+    readonly name : string = '';
+    protected price : number = 0;
+    static readonly DISCOUNT_PERCENT = 10;
+
+    constructor(name?:string,price?:number){
+        this.name = name || "";
+        this.price = price || 0;
+    }
+
+    getPrice(){
+        return this.price;
+    }
+    setPrice(price:number){
+        this.price = price;
+    }
+}
 
